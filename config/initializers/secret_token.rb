623389c0d83aceed7d6269eebb97a8feb90561a2
Tapastreet Developer <houdyerpierre@gmail.com>
2013-12-03 20:33:55 +0000

    first commit and init rails app 4.0 with JSON rendering included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tpoc::Application.config.secret_key_base = '629a77d7956ebf625f06b7270a29a75ad4a73df27598951e7826662622057d1ad941b053e2cb4138c173e89e30c8b8b006cd2ddc9f2b72b5c0d9deb55104ece1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tpoc::Application.config.session_store :cookie_store, key: '_tpoc_session'
